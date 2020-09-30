70623373d8c04461842025f6ad0662cc419d781e
Jacob McCann <mccajd@gmail.com>
2020-09-30 13:15:29 -0400

    Cleaning up family page, fixing duplicate datatables error

--- src/app/list-family/list-family.component.ts
+++ src/app/list-family/list-family.component.ts
@@ -14,91 +14,94 @@ declare interface DataTable {
   styleUrls: ['./list-family.component.css']
 })
 export class ListFamilyComponent implements OnInit {
-  public dataTable: DataTable;
-  ngOnInit(){
-      this.dataTable = {
-          headerRow: [ 'Name', 'Position', 'Office', 'Age', 'Date', 'Actions' ],
-          footerRow: [ 'Name', 'Position', 'Office', 'Age', 'Start Date', 'Actions' ],
-          dataRows: [
-              ['Airi Satou', 'Andrew Mike', 'Develop', '2013', '99,225',''],
-              ['Angelica Ramos', 'John Doe', 'Design', '2012', '89,241', 'btn-round'],
-              ['Ashton Cox', 'Alex Mike', 'Design', '2010', '92,144', 'btn-simple'],
-              ['Bradley Greer','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Brenden Wagner', 'Paul Dickens', 'Communication', '2015', '69,201', ''],
-              ['Brielle Williamson','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Caesar Vance','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Cedric Kelly','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Charde Marshall','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Colleen Hurst','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Dai Rios', 'Andrew Mike', 'Develop', '2013', '99,225',''],
-              ['Doris Wilder', 'John Doe', 'Design', '2012', '89,241', 'btn-round'],
-              ['Fiona Green', 'Alex Mike', 'Design', '2010', '92,144', 'btn-simple'],
-              ['Garrett Winters','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Gavin Cortez', 'Paul Dickens', 'Communication', '2015', '69,201', ''],
-              ['Gavin Joyce','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Gloria Little','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Haley Kennedy','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Herrod Chandler','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Hope Fuentes','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Howard Hatfield', 'Andrew Mike', 'Develop', '2013', '99,225',''],
-              ['Jena Gaines', 'John Doe', 'Design', '2012', '89,241', 'btn-round'],
-              ['Jenette Caldwell', 'Alex Mike', 'Design', '2010', '92,144', 'btn-simple'],
-              ['Jennifer Chang','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Martena Mccray', 'Paul Dickens', 'Communication', '2015', '69,201', ''],
-              ['Michael Silva','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Michelle House','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Paul Byrd','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Prescott Bartlett','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Quinn Flynn','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Rhona Davidson', 'Andrew Mike', 'Develop', '2013', '99,225',''],
-              ['Shou Itou', 'John Doe', 'Design', '2012', '89,241', 'btn-round'],
-              ['Sonya Frost', 'Alex Mike', 'Design', '2010', '92,144', 'btn-simple'],
-              ['Suki Burks','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Tatyana Fitzpatrick', 'Paul Dickens', 'Communication', '2015', '69,201', ''],
-              ['Tiger Nixon','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Timothy Mooney','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Unity Butler','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Vivian Harrell','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round'],
-              ['Yuri Berry','Mike Monday', 'Marketing', '2013', '49,990', 'btn-round']
-          ]
-       };
-  }
+    public dataTable: DataTable;
+    ngOnInit(){
+        this.dataTable = {
+            headerRow: [ 'Name', 'Contact Name', 'Email', 'Family Size', 'Budget', 'Actions' ],
+            footerRow: [ 'Name', 'Contact Name', 'Email', 'Family Size', 'Budget', 'Actions' ],
+            dataRows: [
+                ['Airi Satou', 'Jacob McCann', 'satoua@gmail.com', '4', '$990',''],
+                ['Angelica Ramos', 'Jacob McCann', 'emailplaceholder@mail.com', '2', '$620', 'btn-round'],
+                ['Ashton Cox', 'Angel Selva-Rodriguez', 'emailplaceholder@mail.com', '5', '$730', 'btn-simple'],
+                ['Bradley Greer','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Brenden Wagner', 'Jonathan Cordova', 'examplemail@mail.org', '4', '$450', ''],
+                ['Brielle Williamson','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Caesar Vance','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Cedric Kelly','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Charde Marshall','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Colleen Hurst','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Dai Rios', 'Jacob McCann', 'mailingexample@mail.net', '3', '-$130',''],
+                ['Doris Wilder', 'Jacob McCann', 'emailplaceholder@mail.com', '2', '$620', 'btn-round'],
+                ['Fiona Green', 'Angel Selva-Rodriguez', 'emailplaceholder@mail.com', '5', '$730', 'btn-simple'],
+                ['Garrett Winters','Jimmy Pena', 'testmail@mail.com', '6', '$1,000', 'btn-round'],
+                ['Gavin Cortez', 'Jonathan Cordova', 'examplemail@mail.org', '4', '$450', ''],
+                ['Gavin Joyce','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Gloria Little','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Haley Kennedy','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Herrod Chandler','Jimmy Pena', 'testmail@mail.com', '6', '$1,000', 'btn-round'],
+                ['Hope Fuentes','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Howard Hatfield', 'Jacob McCann', 'mailingexample@mail.net', '3', '-$130',''],
+                ['Jena Gaines', 'Jacob McCann', 'emailplaceholder@mail.com', '2', '$620', 'btn-round'],
+                ['Jenette Caldwell', 'Angel Selva-Rodriguez', 'emailplaceholder@mail.com', '5', '$730', 'btn-simple'],
+                ['Jennifer Chang','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Martena Mccray', 'Jonathan Cordova', 'examplemail@mail.org', '4', '$450', ''],
+                ['Michael Silva','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Michelle House','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Paul Byrd','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Prescott Bartlett','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Quinn Flynn','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Rhona Davidson', 'Jacob McCann', 'mailingexample@mail.net', '3', '-$130',''],
+                ['Shou Itou', 'Jacob McCann', 'emailplaceholder@mail.com', '2', '$620', 'btn-round'],
+                ['Sonya Frost', 'Angel Selva-Rodriguez', 'emailplaceholder@mail.com', '5', '$730', 'btn-simple'],
+                ['Suki Burks','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Tatyana Fitzpatrick', 'Jonathan Cordova', 'examplemail@mail.org', '4', '$450', ''],
+                ['Tiger Nixon','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Timothy Mooney','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Unity Butler','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Vivian Harrell','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round'],
+                ['Yuri Berry','Jimmy Pena', 'testmail@mail.com', '3', '$1,000', 'btn-round']
+            ]
+         };
+    }
 
-  ngAfterViewInit(){
-    $('#datatable').DataTable({
-      "pagingType": "full_numbers",
-      "lengthMenu": [
-        [10, 25, 50, -1],
-        [10, 25, 50, "All"]
-      ],
-      responsive: true,
-      language: {
-        search: "_INPUT_",
-        searchPlaceholder: "Search records",
-      }
+    ngAfterViewInit(){
+      $('#familytable').DataTable({
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+          [10, 25, 50, -1],
+          [10, 25, 50, "All"]
+        ],
+        responsive: true,
+        language: {
+          search: "_INPUT_",
+          searchPlaceholder: "Search records",
+        }
 
-    });
+      });
 
-    var table = $('#datatable').DataTable();
+      var table = $('#familytable').DataTable();
 
-    // Edit record
-    table.on('click', '.edit', function() {
-      let $tr = $(this).closest('tr');
+      // Edit record
+      table.on('click', '.edit', function() {
+        let $tr = $(this).closest('tr');
 
-      var data = table.row($tr).data();
-      alert('You press on Row: ' + data[0] + ' ' + data[1] + ' ' + data[2] + '\'s row.');
-    });
+        var data = table.row($tr).data();
+        alert('Click here to view or edit ' + data[0] + '\'s information.');
+      });
 
-    // Delete a record
-    table.on('click', '.remove', function(e) {
-      let $tr = $(this).closest('tr');
-      table.row($tr).remove().draw();
-      e.preventDefault();
-    });
+      // Archive a record.
+      table.on('click', '.archive', function(e) {
+        var c = confirm("Are you sure you wish to archive this family?");
+        if(c) {
+          let $tr = $(this).closest('tr');
+          table.row($tr).remove().draw();
+          e.preventDefault();
+        }
+      });
 
-    //Like record
-    table.on('click', '.like', function() {
-      alert('You clicked on Like button');
-    });
-  }
+      // View record requests.
+      table.on('click', '.service', function() {
+        alert('Click here to view or add service requests.');
+      });
+    }
 }
